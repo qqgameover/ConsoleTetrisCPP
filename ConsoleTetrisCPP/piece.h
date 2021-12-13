fd4e1ccf060ad5219b7f1355a1f37acc39e78264
qqgameover <kasper35@live.no>
2021-12-13 16:25:08 +0100

    Update piece.h

--- ConsoleTetrisCPP/piece.h
+++ ConsoleTetrisCPP/piece.h
@@ -8,15 +8,15 @@ using namespace std;
 
 class piece
 {
-	static vector<vector<unsigned char>> GetRandomBlockMatrix();
-public:
+    static vector<vector<unsigned char>> GetRandomBlockMatrix();
+    public:
     std::unique_ptr<position> Position;
-	vector<vector<unsigned char>> blockMatrix;
-	piece();
-	void MoveBlock(position dir, std::vector<std::vector<unsigned char>> landedArray);
-	void UnDrawBlock(board &gameBoards);
-	void DrawBlock(board &gameBoards);
-	static void RotateBlock(std::vector<std::vector<unsigned char>> &outBlockMatrix);
+    vector<vector<unsigned char>> blockMatrix;
+    piece();
+    void MoveBlock(position dir, std::vector<std::vector<unsigned char>> landedArray);
+    void UnDrawBlock(board &gameBoards);
+    void DrawBlock(board &gameBoards);
+    static void RotateBlock(std::vector<std::vector<unsigned char>> &outBlockMatrix);
     bool IsSidesColliding(std::vector<std::vector<unsigned char>> landedArray, position dir);
 };
 
