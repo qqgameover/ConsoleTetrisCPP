91647b6d99900b9b72f6f5bf7ec2181fab5d204d
qqgameover <kasper35@live.no>
2021-12-14 21:45:48 +0100

    Fixed some indentation

--- ConsoleTetrisCPP/piece.h
+++ ConsoleTetrisCPP/piece.h
@@ -6,21 +6,22 @@
 
 class piece
 {
-    static std::vector<std::vector<unsigned char>> GetRandomBlockMatrix();
+    private:
+        static std::vector<std::vector<unsigned char>> GetRandomBlockMatrix();
     public:
-    std::unique_ptr<position> Position;
-    std::vector<std::vector<unsigned char>> blockMatrix;
-    piece();
-    void MoveBlock(position dir, std::vector<std::vector<unsigned char>> landedArray);
-    void UnDrawBlock(board &gameBoards);
-    void DrawBlock(board &gameBoards);
-    void WallKick(std::vector<std::vector<unsigned char>> tempVec);
-    void RotateBlock(std::vector<std::vector<unsigned char>> &outBlockMatrix, 
-            std::vector<std::vector<unsigned char>> &landedArray, position dir);
-    bool IsSidesColliding(std::vector<std::vector<unsigned char>> landedArray, position dir);
-    bool TestRotation(std::vector<std::vector<unsigned char>> &landedArray, position dir, 
-            std::vector<std::vector<unsigned char>> tempVec);
-    bool TestAllRotations(std::vector<std::vector<unsigned char>> &landedArray);
-    std::vector<position> GetBlockSegments(position currentPos, std::vector<std::vector<unsigned char>> tempVec);
+        std::unique_ptr<position> Position;
+        std::vector<std::vector<unsigned char>> blockMatrix;
+        piece();
+        void MoveBlock(position dir, std::vector<std::vector<unsigned char>> landedArray);
+        void UnDrawBlock(board &gameBoards);
+        void DrawBlock(board &gameBoards);
+        void WallKick(std::vector<std::vector<unsigned char>> tempVec);
+        void RotateBlock(std::vector<std::vector<unsigned char>> &outBlockMatrix, 
+                std::vector<std::vector<unsigned char>> &landedArray, position dir);
+        bool IsSidesColliding(std::vector<std::vector<unsigned char>> landedArray, position dir);
+        bool TestRotation(std::vector<std::vector<unsigned char>> &landedArray, position dir, 
+                std::vector<std::vector<unsigned char>> tempVec);
+        bool TestAllRotations(std::vector<std::vector<unsigned char>> &landedArray);
+        std::vector<position> GetBlockSegments(position currentPos, std::vector<std::vector<unsigned char>> tempVec);
 };
 
