fb2347b824ac5ce045515aa366b1668774be4a52
qqgameover <kasper35@live.no>
2021-12-14 22:47:18 +0100

    x

--- ConsoleTetrisCPP/piece.h
+++ ConsoleTetrisCPP/piece.h
@@ -6,8 +6,12 @@
 
 class piece
 {
-    private:
-        static std::vector<std::vector<unsigned char>> GetRandomBlockMatrix();
+    static std::vector<std::vector<unsigned char>> GetRandomBlockMatrix();
+    void WallKick(std::vector<std::vector<unsigned char>> &tempVec);
+    bool TestRotation(std::vector<std::vector<unsigned char>> &landedArray, position dir, 
+            std::vector<std::vector<unsigned char>> &tempVec);
+    bool TestAllRotations(std::vector<std::vector<unsigned char>> &landedArray);
+    std::vector<position> GetBlockSegments(position currentPos, std::vector<std::vector<unsigned char>> &tempVec);
     public:
         std::unique_ptr<position> Position;
         std::vector<std::vector<unsigned char>> blockMatrix;
@@ -15,13 +19,8 @@ class piece
         void MoveBlock(position dir, std::vector<std::vector<unsigned char>> landedArray);
         void UnDrawBlock(board &gameBoards);
         void DrawBlock(board &gameBoards);
-        void WallKick(std::vector<std::vector<unsigned char>> &tempVec);
         void RotateBlock(std::vector<std::vector<unsigned char>> &outBlockMatrix, 
                 std::vector<std::vector<unsigned char>> &landedArray, position dir);
         bool IsSidesColliding(std::vector<std::vector<unsigned char>> landedArray, position dir);
-        bool TestRotation(std::vector<std::vector<unsigned char>> &landedArray, position dir, 
-                std::vector<std::vector<unsigned char>> &tempVec);
-        bool TestAllRotations(std::vector<std::vector<unsigned char>> &landedArray);
-        std::vector<position> GetBlockSegments(position currentPos, std::vector<std::vector<unsigned char>> &tempVec);
 };
 
