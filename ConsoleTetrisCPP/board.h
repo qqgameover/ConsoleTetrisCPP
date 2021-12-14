e3fe200952e19ea13656d0155ad5e29d39a7c86d
qqgameover <kasper35@live.no>
2021-12-14 14:56:15 +0100

    Changed to an std array

--- ConsoleTetrisCPP/board.h
+++ ConsoleTetrisCPP/board.h
@@ -1,12 +1,13 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <array>
 class board
 {
 public:
     std::vector<std::vector<unsigned char>> BoardArray;
     std::vector<std::vector<unsigned char>> LandedArray;
-	const std::string colorarray[8] = { "30", "31", "32", "33", "34", "35", "35", "37" };
+    std::array<std::string, 8> colorarray = { "30", "31", "32", "33", "34", "35", "35", "37" };
     int Points;
 	board();
 	void DrawBoard();
