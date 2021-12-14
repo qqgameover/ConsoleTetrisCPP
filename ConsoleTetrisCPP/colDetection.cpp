6fdc8abcbfc08d4b3db9b1ad49250626791cefa0
qqgameover <kasper35@live.no>
2021-12-14 20:17:05 +0100

    x

--- ConsoleTetrisCPP/colDetection.cpp
+++ ConsoleTetrisCPP/colDetection.cpp
@@ -5,11 +5,11 @@ bool ColDetection::IsColliding(std::vector<std::vector<unsigned char>> &landedAr
 	const size_t height = piece.blockMatrix.size();
 	const size_t width = piece.blockMatrix[0].size();
 	for (size_t yIndex = 0; yIndex < height; ++yIndex)
-	for (size_t xIndex = 0; xIndex < width; ++xIndex)
-	{
-		if (piece.blockMatrix[yIndex][xIndex] == 0) continue;
-		if (landedArray[yIndex + piece.Position->y + dir.y][xIndex + piece.Position->x + dir.x] == 0) continue;
-		return true;
-	}
+        for (size_t xIndex = 0; xIndex < width; ++xIndex)
+        {
+            if (piece.blockMatrix[yIndex][xIndex] == 0) continue;
+            if (landedArray[yIndex + piece.Position->y + dir.y][xIndex + piece.Position->x + dir.x] == 0) continue;
+            return true;
+        }
 	return false;
 }
