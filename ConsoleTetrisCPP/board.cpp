1782321bd653182be4afa6872ae369aa60de0dbb
qqgameover <kasper35@live.no>
2021-12-19 19:06:58 +0100

    indentation again

--- ConsoleTetrisCPP/board.cpp
+++ ConsoleTetrisCPP/board.cpp
@@ -2,47 +2,47 @@
 #include <iostream>
 board::board() 
 {
-	BoardArray = {{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 
-			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-			{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
-	copy(BoardArray.begin(), BoardArray.end(), back_inserter(LandedArray));
+    BoardArray = {{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+    copy(BoardArray.begin(), BoardArray.end(), back_inserter(LandedArray));
     Points = 0;
 }
 void board::DrawBoard()
 {
-	size_t height = BoardArray.size();
-	size_t width = BoardArray[0].size();
-	for (size_t yAxis = 0; yAxis < height; yAxis++)
-	for (size_t xAxis = 0; xAxis < width; xAxis++)
-	{
-		if (BoardArray[yAxis][xAxis] == 0) std::cout << "\033[1;36m+\033[0m" << " ";
-		else
-		{
-			std::cout << "\033[1;" << colorarray[BoardArray[yAxis][xAxis] - 1] << 
-				"m" << static_cast<int>(BoardArray[yAxis][xAxis]) << "\033[0m" << " ";
-		}
-		if (xAxis == width - 1) std::cout << "\n";
-	}
+    size_t height = BoardArray.size();
+    size_t width = BoardArray[0].size();
+    for (size_t yAxis = 0; yAxis < height; yAxis++)
+        for (size_t xAxis = 0; xAxis < width; xAxis++)
+        {
+            if (BoardArray[yAxis][xAxis] == 0) std::cout << "\033[1;36m+\033[0m" << " ";
+            else
+            {
+                std::cout << "\033[1;" << colorarray[BoardArray[yAxis][xAxis] - 1] << 
+                    "m" << static_cast<int>(BoardArray[yAxis][xAxis]) << "\033[0m" << " ";
+            }
+            if (xAxis == width - 1) std::cout << "\n";
+        }
     std::cout<< "Total points:" << Points;
 }
 void board::CheckForTetris()
@@ -59,9 +59,9 @@ void board::CheckForTetris()
             if(BoardArray[yAxis][xAxis] == 0) fullLine = false;
         }
         if(!fullLine) continue;
-		BoardArray.erase(BoardArray.begin() + yAxis);
-		BoardArray.insert(BoardArray.begin() + 1, std::vector<unsigned char> {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1});
-		linesCleared++;
+        BoardArray.erase(BoardArray.begin() + yAxis);
+        BoardArray.insert(BoardArray.begin() + 1, std::vector<unsigned char> {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1});
+        linesCleared++;
     }
     Points += linesCleared;
 }
